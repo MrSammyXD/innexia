315ac3c4e3af4caa3bf1d16288cd7f03ce62b8fd
☞丂ﾑﾶﾶƳ☜ <76897487+MrSammyXD@users.noreply.github.com>
2021-03-02 13:20:49 +0530

    Update __main__.py

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -184,7 +184,7 @@ def test(update, context):
 
 
 @run_async
-def donate(update: update, context):
+def donate(update: context):
     user = update.effective_message.from_user
     chat = update.effective_chat  # type: Optional[Chat]
 
