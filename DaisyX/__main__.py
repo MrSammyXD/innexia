080f683e57d582d46bd97cfa4c5efab559c0fea1
☞丂ﾑﾶﾶƳ☜ <76897487+MrSammyXD@users.noreply.github.com>
2021-03-09 14:00:51 +0530

    Update __main__.py

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -63,7 +63,7 @@ from DaisyX.modules.helper_funcs.misc import paginate_modules
 from DaisyX.modules.helper_funcs.readable_time import get_readable_time
 
 PM_START_TEXT = """
-Hello There, I'm [innexia](https://telegra.ph/file/be8203902f20e1e16a9e7.jpg)
+Hello There, I'm [innexia](https://telegra.ph/file/7ed50dcd5c1346bb6dc47.jpg)
 
 An Advanced Telegram Group Management Bot For All Your Needs !!
 """
